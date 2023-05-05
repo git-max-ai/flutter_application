6efe8b8ccdea9b754ab2c0120d2ba5c9875b80bd
Kanak Jadaun <75478908+git-max-ai@users.noreply.github.com>
2023-05-05 19:13:03 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// import 'pages/home_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // int days = 30;
+    // String name = "Coding";
+    // double pi = 3.14;
+    // num temp = 30.5; // use for both int and double
+    // bool isMale = false;
+
+    var day = "Tuesday"; // compiler will decide the data type
+    const pi = 3.14; // value will not change
+    // final - in this we can make changes
+
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      // primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+
+      // initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "home": (context)  => HomePage(),
+        "/Login": (context) => LoginPage(),
+      },
+    );
+  }
+}
