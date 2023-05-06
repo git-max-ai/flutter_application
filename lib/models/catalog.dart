fa2023bb3891bd4fcab5db427425e90f0a3da359
Kanak Jadaun <75478908+git-max-ai@users.noreply.github.com>
2023-05-06 19:16:20 +0530

    list

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,18 +1,7 @@
-
-class Item{
-  final String id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
-
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
-}
-
-final products = [
+class CatalogModel{
+  static final items = [
   Item(
-      id: "Codepur001",
+      id: 1,
       name: "iPhone 12 Pro",
       desc: "Apple iPhone 12th generation",
       price: 999,
@@ -20,3 +9,16 @@ final products = [
       image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
 ];
+}
+
+class Item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+
